f0191fa50e80a135c0b2ba0e1e0dc1140bba880b
consy98@gmail.com <Cons.proj98>
2023-04-04 19:23:23 +0200

    Initial Commit

---
+++ Little Lemon iOS Capstone/Little_Lemon_iOS_CapstoneApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_Lemon_iOS_CapstoneApp.swift
+//  Little Lemon iOS Capstone
+//
+//  Created by Andrea Consorti on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_iOS_CapstoneApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
